3770e900988999e7c6b53d7e317fd4a2bfaebc17
Php <85707347+Php22@users.noreply.github.com>
2024-10-15 19:11:41 -0300

    Create index.php

---
+++ docs/src/lotes_estoque/index.php
@@ -0,0 +1,56 @@
+class Estoque {
+    private $idEstoque;
+    private $fornecedor;
+    private $placaRefletiva;
+    private $caixaCarro;
+    private $caixaMoto;
+
+    public function __construct($idEstoque, $fornecedor, $placaRefletiva, $caixaCarro, $caixaMoto) {
+        $this->idEstoque = $idEstoque;
+        $this->fornecedor = $fornecedor;
+        $this->placaRefletiva = $placaRefletiva;
+        $this->caixaCarro = $caixaCarro;
+        $this->caixaMoto = $caixaMoto;
+    }
+
+    // Getters e Setters
+    public function getIdEstoque() {
+        return $this->idEstoque;
+    }
+
+    public function setIdEstoque($idEstoque) {
+        $this->idEstoque = $idEstoque;
+    }
+
+    public function getFornecedor() {
+        return $this->fornecedor;
+    }
+
+    public function setFornecedor($fornecedor) {
+        $this->fornecedor = $fornecedor;
+    }
+
+    public function getPlacaRefletiva() {
+        return $this->placaRefletiva;
+    }
+
+    public function setPlacaRefletiva($placaRefletiva) {
+        $this->placaRefletiva = $placaRefletiva;
+    }
+
+    public function getCaixaCarro() {
+        return $this->caixaCarro;
+    }
+
+    public function setCaixaCarro($caixaCarro) {
+        $this->caixaCarro = $caixaCarro;
+    }
+
+    public function getCaixaMoto() {
+        return $this->caixaMoto;
+    }
+
+    public function setCaixaMoto($caixaMoto) {
+        $this->caixaMoto = $caixaMoto;
+    }
+}
